6843fd5cb4d80a0931c6132ccdc6a6bb376778b3
Ma1y0 <matyas.barr@gmail.com>
2023-01-29 14:00:40 +0100

    post Router error handling

--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -5,65 +5,100 @@ const router = Router()
 
 // Get all Posts
 router.get("/", async (req, res) => {
-    const posts = await prisma.post.findMany()
-    res.json(posts)
+    try {
+        const posts = await prisma.post.findMany()
+        res.json(posts)
+    } catch (err) {
+        res.json({
+            error: err
+        })
+    }
+
 })
 
 // Get Post by Id
 router.get("/:id", async (req, res) => {
     const { id } = req.params
-    const post = await prisma.post.findFirst({
-        where: {
-            id: id
-        }
-    })
-    res.json(post)
+
+    try {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: id
+            }
+        })
+
+        res.json(post)
+    } catch (err) {
+        res.json({
+            error: err
+        })
+    }
 })
 
 // Create new Post
 router.post("/", async (req, res) => {
     const body = req.body
-    const post = await prisma.post.create({
-        data: {
-            title: body.title,
-            content: body.content,
-            author: {
-                connect: {
-                  id: body.authorId
+
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title: body.title,
+                content: body.content,
+                author: {
+                    connect: {
+                      id: body.authorId
+                    }
                 }
             }
-        }
-    })
+        })
 
-    res.json(post)
+        res.json(post)
+    } catch (err) {
+        res.json({
+            error: err
+        })
+    }
+
 })
 
 // Update Post
 router.put("/:id", async (req, res) => {
     const { id } = req.params
     const body = req.body
-    const post = await prisma.post.update({
-        where: {
-            id: id
-        },
-        data: body
-    })
 
-    res.json({
-        post
-    })
+    try {
+        const post = await prisma.post.update({
+            where: {
+                id: id
+            },
+            data: body
+        })
+    
+        res.json(post)
+    } catch (err) {
+        res.json({
+            error: err
+        })
+    }
 })
 
 // Delete Post
 router.delete("/:id", async (req, res) => {
     const { id } = req.params
-    const post = await prisma.post.delete({
-        where: {
-            id: id
-        }
-    })
 
-    res.json(post)
+    try {
+        const post = await prisma.post.delete({
+            where: {
+                id: id
+            }
+        })
+    
+        res.json(post)
+    } catch (err) {
+        res.json({
+            error: err
+        })
+    }
 })
 
 export {
